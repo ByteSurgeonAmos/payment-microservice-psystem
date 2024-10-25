b6f106ce9cc5cd4b7b7cf28a6d8eb15e6dec839a
ByteSurgeonAmos <wachiraamos402@gmail.com>
2024-10-25 14:26:01 +0300

    chore:add comprehensive documentation

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
